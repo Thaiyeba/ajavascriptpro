fc8224c9d4585a05217b5baaef974aa63d5956e5
Thaiyeba <133875794+Thaiyeba@users.noreply.github.com>
2025-06-19 21:42:34 +0530

    Update control_flow.js

--- controlflow/control_flow.js
+++ controlflow/control_flow.js
@@ -32,6 +32,9 @@ switch (userType) {
         break;
     case "subscriber":
         userCategory = "Subscriber";
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
         break;
     default:
         userCategory = "Unknown";
